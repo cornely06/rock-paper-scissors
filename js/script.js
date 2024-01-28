d80cedfdbc68e8120e55a07bed90e64f99ce000c
rc <59358349+cornely06@users.noreply.github.com>
2024-01-28 01:02:10 -0500

    Create function to generate number between 1 and 3

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Get random number between 1 and 3
+function randomNumber() {
+  return Math.floor(Math.random() * 3) + 1
+}
+// Get computer choice
+function getComputerChoice() {
+  
+}
+// Input player choice
+// Compare and return results
